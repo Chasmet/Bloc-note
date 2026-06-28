c62c3b261cbe851bca290c0c872a4cced8714b89
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-06-28 13:22:52 +0200

    Force la mise à jour du cache application

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'bloc-note-v1';
+const CACHE_NAME = 'bloc-note-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './logo.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
